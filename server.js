4439fda48954e6c680a480edbed4fdf9a8065ba0
jlangbid13 <112040145+jlangbid13@users.noreply.github.com>
2023-12-13 22:30:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const argon2 = require('argon2'); // Import argon2
-
+const path = require('path');
 const scheduleRoutes = require('./routes/scheduleRoutes');
 
 const app = express();
