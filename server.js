8b92e9e63f986435bb107250adc461dfae9809ea
jlangbid13 <112040145+jlangbid13@users.noreply.github.com>
2023-12-13 22:58:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "./frontend/build"));
+app.use(express.static(path.join(__dirname, "./frontend/build"))}
 
 // Schedule routes
 app.use('/api/schedule', scheduleRoutes);
